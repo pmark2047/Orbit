fae03e550892965b3d69cbf77ceed17fe8ce01bc
Peyton Markus <150070019+pmark2047@users.noreply.github.com>
2025-11-15 11:00:08 -0700

    Added files

---
+++ orbit.cpp
@@ -0,0 +1,33 @@
+/**********************************************************************
+* Source File:
+*    Orbit Lab
+* Author:
+*    Peyton and Jackson
+* Summary:
+*    Orbit
+************************************************************************/
+
+#include "test.h"
+#include <iostream>
+using namespace std;
+
+/*********************************
+ * MAIN - Where it all begins...
+ *********************************/
+#ifdef _WIN32
+#include <windows.h>
+int WINAPI WinMain(
+   _In_ HINSTANCE hInstance,
+   _In_opt_ HINSTANCE hPrevInstance,
+   _In_ PSTR pCmdLine,
+   _In_ int nCmdShow)
+#else // !_WIN32
+int main(int argc, char** argv)
+#endif // !_WIN32
+{
+
+   // run all the unit tests
+   testRunner();
+   
+   return 0;
+}
---
+++ test.h
@@ -0,0 +1,12 @@
+/***********************************************************************
+ * Heaader File:
+ *    TEST
+ * Author:
+ *    Peyton and Jackson
+ * Summary:
+ *    The test runner for all the unit tests
+ ************************************************************************/
+
+#pragma once
+
+void testRunner();
