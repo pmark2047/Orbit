55c39444f170ebb79bd61b0fabb8b52db374defd
Peyton Markus <150070019+pmark2047@users.noreply.github.com>
2025-11-15 11:06:11 -0700

    Add files via upload

---
+++ OrbitLab/testOrbit.cpp
@@ -0,0 +1,18 @@
+/***********************************************************************
+ * Source File:
+ *    TEST ORBIT
+ * Author:
+ *    Peyton and Jackson
+ * Summary:
+ *    The test for ORBIT
+ ************************************************************************/
+
+#include "testOrbit.h"
+#include <cassert>
+
+// ex. void TestOrbit::getMoves_blocked()
+
+   // SETUP
+   // EXERCISE
+   // VERIFY
+   // TEARDOWN
---
+++ OrbitLab/testOrbit.h
@@ -0,0 +1,32 @@
+/***********************************************************************
+ * Heaader File:
+ *    TEST ORBIT
+ * Author:
+ *    Peyton and Jackson
+ * Summary:
+ *    The test for ORBIT
+ ************************************************************************/
+
+#pragma once
+
+#include "unitTest.h"
+
+/***************************************************
+ * ORBIT TEST
+ * Test the Orbit class
+ ***************************************************/
+class TestOrbit : public UnitTest
+{
+public:
+
+   void run()
+   {
+      // ex. getMoves_blocked();
+      // ex. getMoves_slideToEnd();
+      
+      // report("Orbit");
+   }
+private:
+   // ex. void getMoves_blocked();
+   // ex. void getMoves_slideToEnd();
+};
